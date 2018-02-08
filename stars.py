a4b9789ce72116a3c42dc06bb911b991ec154375
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-02-07 22:21:33 -0500

    Update stars.py

--- stars.py
+++ stars.py
@@ -9,7 +9,7 @@ For example:
 x = [4, 6, 1, 3, 5, 7, 25]
 draw_stars(x) should print the following when invoked:"""
 
-x = [4, 6, 1, 3, 5, 7, 25]
+"""x = [4, 6, 1, 3, 5, 7, 25]
 
 def draw_stars(x):
     star = "*"
@@ -18,6 +18,24 @@ def draw_stars(x):
         print i
 
 draw_stars(x)
+
+Part II
+Modify the function above. Allow a list containing integers and strings to be passed to the draw_stars() function. When a string is passed, instead of displaying *, display the first letter of the string according to the example below. You may use the .lower() string method for this part."""
+
+x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
+
+def draw_stars(x):
+    star = "*"
+
+    for i in x:
+        if type(i) == int:
+            i *= star
+            print i
+        if type(i) == str:
+            i = i[0].lower() * len(i)
+            print i
+
+draw_stars(x)  
 
         
 
