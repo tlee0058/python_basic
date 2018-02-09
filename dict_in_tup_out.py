c56e72c1224ec2e0a7b3b13fb47f1f9293877721
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-02-08 19:40:10 -0500

    Add files via upload

---
+++ dict_in_tup_out.py
@@ -0,0 +1,19 @@
+"""Assignment: Dictionary in, tuples out
+Write a function that takes in a dictionary and returns a list of tuples where the first tuple item is the key and the second is the value. Here's an example:
+
+function input"""
+
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+tup = tuple(my_dict.items())
+
+print tup
+
+
+
+
+
