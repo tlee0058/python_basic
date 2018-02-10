66e86d76aaf04bfb7fb0206eb1cade4a5731841b
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-02-10 11:03:23 -0500

    Update making_dict.py

--- making_dict.py
+++ making_dict.py
@@ -24,6 +24,31 @@ def make_dict(list1, list2):
 
 make_dict(name, favorite_animal)
 
+def printDict(obj):
+    # print all keys and values in dictionary
+    for key,value in obj.iteritems():
+        print "My", key,"is", value
+
+    #look at other ways to print dictionary
+    print "\n", "---- other ways to print dictionary ----"
+
+    #print all keys
+    print "\n", "Print all keys:"
+    for data in obj:
+        print data
+
+    #another way to print all keys
+    print "\n", "Print all keys another way:"
+    for obj_key in obj.iterkeys():
+        print obj_key
+
+    #to print the values
+    print "\n", "Print all values:"
+    for val in obj.itervalues():
+        print val
+
+printDict(myself)
+
 
 
 
