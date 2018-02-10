c23633a72e87f80de3081221a5776199649ba23c
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-02-10 10:31:16 -0500

    Update compare_lists.py

--- compare_lists.py
+++ compare_lists.py
@@ -1,16 +1,10 @@
-# Assignment: Type List
-# Write a program that takes a list and prints a message for each element in the list, based on that element's data type.
+# # Assignment: Type List
+# # Write a program that takes a list and prints a message for each element in the list, based on that element's data type.
 
-# Your program input will always be a list. For each item in the list, test its data type. If the item is a string, concatenate it onto a new string. If it is a number, add it to a running sum. At the end of your program print the string, the number and an analysis of what the list contains. If it contains only one type, print that type, otherwise, print 'mixed'.
+# # Your program input will always be a list. For each item in the list, test its data type. If the item is a string, concatenate it onto a new string. If it is a number, add it to a running sum. At the end of your program print the string, the number and an analysis of what the list contains. If it contains only one type, print that type, otherwise, print 'mixed'.
 
-# Here are a couple of test cases. Think of some of your own, too. What kind of unexpected input could you get?
+# # Here are a couple of test cases. Think of some of your own, too. What kind of unexpected input could you get?
 
-# #input
-# l = ['magical unicorns',19,'hello',98.98,'world']
-# #output
-# "The list you entered is of mixed type"
-# "String: magical unicorns hello world"
-# "Sum: 117.98"
 # Assignment: Compare Lists
 # Write a program that compares two lists and prints a message depending on if the inputs are identical or not.
 
@@ -18,6 +12,21 @@
 
 list_one = [1,2,5,6,2]
 list_two = [1,2,5,6,2]
+# list_one = [1,2,5,6,5]
+# list_two = [1,2,5,6,5,3]
+# list_one = [1,2,5,6,5,16]
+# list_two = [1,2,5,6,5]
+# list_one = ['celery','carrots','bread','milk']
+# list_two = ['celery','carrots','bread','cream']
 
-if (list_one == list_two):
-    print("same")
+def compare_list(list_one, list_two):
+    if not len(list_one) == len(list_two):
+        print "The lists are not the same"
+    else:
+        for i in range(0, len(list_one)):
+            if not list_one[i] == list_two[i]:
+                print "The lists are not the same"
+                return
+        print "The lists are the same"
+
+compare_list(list_two, list_one)
