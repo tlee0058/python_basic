da03739d945e865342caf61c312408e8dae17f00
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-02-09 21:19:46 -0500

    Update names.py

--- names.py
+++ names.py
@@ -22,6 +22,31 @@ KB Tonel"""
 for i in students:
     print i['first_name'] + " " + i['last_name']
 
+
+#Part 2
+users = {
+ 'Students': [ 
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key, data in users.items():
+	counter = 0
+	for value in data:
+		counter = counter +1
+		full_name = value["first_name"] + " " + value["last_name"]
+		full_name_upper = full_name.upper()
+		name_count = len(value["first_name"]) + len(value["last_name"])
+		
+		print counter, '-',full_name_upper, name_count
+
         
 
 
